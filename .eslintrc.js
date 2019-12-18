2b30fe0cee16974768f8feb01d28028c051fb61f
Dan Dombrowski <djdmbrwsk@gmail.com>
2019-12-17 20:06:07 -0500

    Allow function usage before definition

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,4 +5,7 @@ module.exports = {
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
+  rules: {
+    '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+  },
 };
